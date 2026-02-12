438c677d9064fd2167985e2d60c17f3e375b8b44
Dhanu-coder-indian <shreyasd489@gmail.com>
2026-02-12 11:58:10 +0530

    Updated PORT config for Railway

--- backend/server.js
+++ backend/server.js
@@ -358,7 +358,8 @@ app.get('/chart-data/yearly/:year', auth, (req, res) => {
 /* =========================
    SERVER START
 ========================= */
-app.listen(5000, () => {
-  console.log("✅ Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
