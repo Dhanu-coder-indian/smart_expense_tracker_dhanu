9eb65b9a68b225905bece619c7c85284d5d2c8e6
Dhanu-coder-indian <shreyasd489@gmail.com>
2026-02-12 11:55:24 +0530

    Updated DB config for Railway

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,19 @@
+const mysql = require("mysql2");
 
-const mysql = require('mysql2');
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'expense_tracker'
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
-db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL Connected");
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to Railway MySQL");
+  }
 });
-module.exports = db;
+
+module.exports = connection;
